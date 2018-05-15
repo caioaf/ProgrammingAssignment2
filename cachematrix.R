9ea7e4aa3422c12baacf249f8c0f2da0b2f03b05
Caio Alves Furtado <caioaf@mac-caioaf.global.cadence.com>
2018-05-15 18:22:08 -0300

    Coursera activity

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Script contains methods to create special matrix objects and 
+## to calculate the inverse of said matrix
 
-## Write a short comment describing this function
-
+## Creates an object with a matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns the inverse value of a matrix
+## If inverse was not cached yet, it also calculates and caches it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if (!is.null(i)) {
+        return(i)
+    }
+    
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
